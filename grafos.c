b07beab9c3b134fc275ee7905905ca86eb5f2ad4
root <root@NOTE-PEDRO.atlantica.biz>
2024-03-15 19:31:01 -0300

    adicionado nós

--- grafos.c
+++ grafos.c
@@ -1,14 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
     int data;
-    struct node* left;
-    struct node* right;
+    struct node *left;
+    struct node *right;
 };
 
-
-int main(int argc, char const *argv[])
+// newNode() allocates a new node
+// with the given data and NULL left
+// and right pointers.
+struct node *newNode(int data)
 {
-    
+    printf("%d", data);
+    // Allocate memory for new node
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+
+    // Assign data to this node
+    node->data = data;
+
+    // Initialize left and
+    // right children as NULL
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
+int main()
+{
+    // Create root
+    struct node *root = newNode(1);
+
+    /* following is the tree after above statement
+        1
+    / \
+    NULL NULL
+    */
+    root->left = newNode(2);
+    root->right = newNode(3);
+
+    /* 2 and 3 become left and right children of 1
+            1
+        / \
+        2 3
+        / \ / \
+    NULL NULL NULL NULL
+    */
+    root->left->left = newNode(4);
+
+    /* 4 becomes left child of 2
+        1
+        / \
+    2 3
+    / \ / \
+    4 NULL NULL NULL
+    / \
+    NULL NULL
+    */
+    return 0;
 }
