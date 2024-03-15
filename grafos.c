0556a12ebdae84dc8351d93653660a949e45c063
root <root@NOTE-PEDRO.atlantica.biz>
2024-03-15 20:43:21 -0300

    praticamente finalizado

--- grafos.c
+++ grafos.c
@@ -35,13 +35,83 @@ struct TreeNode* insert(struct TreeNode* root, int x)
     return root;
 }
 
+struct TreeNode* findMin(struct TreeNode* root)
+{
+    if(root == NULL)
+    	return NULL;
+    else if(root->left == NULL)
+    	return root;
+    else
+    	return findMin(root->left);
+}
+
+struct TreeNode* findMax(struct TreeNode* root)
+{
+    if(root == NULL)
+    	return NULL;
+    else if(root->right == NULL)
+    	return root;
+    else
+    	return findMax(root->right);
+}
+
+struct TreeNode* find(struct TreeNode* root, int x)
+{
+    if(root == NULL)
+        return NULL;
+    else if(x < root->data)
+        return find(root->left, x);
+    else if(x > root->data)
+        return find(root->right, x);
+    else
+        return root;
+}
+
+struct TreeNode* delete(struct TreeNode* root, int x)
+{
+    struct TreeNode* temp;
+    if(root == NULL)
+        return NULL;
+    else if(x < root->data)
+        root->left = delete(root->left, x);
+    else if(x > root->data)
+        root->right = delete(root->right, x);
+    else if(root->left && root->right)
+    {
+        temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, root->data);
+    }
+    else
+    {
+        temp = root;
+        if(root->left == NULL)
+            root = root->right;
+        else if(root->right == NULL)
+            root = root->left;
+        free(temp);
+    }
+    return root;
+}
+
+void inorder(struct TreeNode* root)
+{
+    if(root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+
 int main(int argc, char const *argv[])
 {
     struct TreeNode* root = insert(NULL, 10);
-    struct TreeNode* node1 = insert(root, 20);
-    {
-        /* data */
-    };
+    insert(root, 20);
     
+    printf("%d", root->right); // Funciona?
+
+    insert(root, 30);
+    insert(root->right, 30);
     return 0;
 }
