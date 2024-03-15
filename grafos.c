d10146f72a35a4da22174f0318cfa7473025a974
root <root@NOTE-PEDRO.atlantica.biz>
2024-03-15 20:13:50 -0300

    inserção funcionando

--- grafos.c
+++ grafos.c
@@ -1,75 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
-struct node
+struct TreeNode
 {
     int data;
-    struct node *left;
-    struct node *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
-// newNode() allocates a new node
-// with the given data and NULL left
-// and right pointers.
-struct node* newNode(int data)
+struct TreeNode* makeEmpty(struct TreeNode* root)
 {
-    printf("%d", data);
-    // Allocate memory for new node
-    struct node *node = (struct node *)malloc(sizeof(struct node));
-
-    // Assign data to this node
-    node->data = data;
-
-    // Initialize left and
-    // right children as NULL
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
-}
-
-struct node* insertNoOrdenado(node* root, int valor)
-{
-    if (root == NULL){
-        root = newNode(valor);
-        return root;
+    if(root != NULL)
+    {
+        makeEmpty(root->left);
+        makeEmpty(root->right);
+        free(root);
     }
-
-    
-
-
+    return NULL;
 }
 
-int main()
+struct TreeNode* insert(struct TreeNode* root, int x)
+{   
+    printf("%d", x);
+    if(root == NULL)
+    {
+        root = malloc(sizeof(struct TreeNode));
+        root->data = x;
+        root->left = root->right = NULL;
+    }
+    else if(x < root->data)
+        root->left = insert(root->left, x);
+    else if(x > root->data)
+        root->right = insert(root->right, x);
+    return root;
+}
+
+int main(int argc, char const *argv[])
 {
-    // Create root
-    struct node *root = newNode(1);
-
-    /* following is the tree after above statement
-        1
-    / \
-    NULL NULL
-    */
-    root->left = newNode(2);
-    root->right = newNode(3);
-
-    /* 2 and 3 become left and right children of 1
-            1
-        / \
-        2 3
-        / \ / \
-    NULL NULL NULL NULL
-    */
-    root->left->left = newNode(4);
-
-    /* 4 becomes left child of 2
-        1
-        / \
-    2 3
-    / \ / \
-    4 NULL NULL NULL
-    / \
-    NULL NULL
-    */
+    struct TreeNode* root = insert(NULL, 10);
+    struct TreeNode* node1 = insert(root, 20);
+    {
+        /* data */
+    };
+    
     return 0;
 }
